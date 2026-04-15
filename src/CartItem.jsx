13cfd3b28c94041f9648e540977fcacdb20353f6
tmz_0x <thisalmethwidu16@gmail.com>
2026-04-15 09:43:35 +0530

    CartItems component updated

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,46 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+          const total =0;
+          cart.forEach(item => {
+               
+               parseFloat(item.cost.substtring(1));
+               total += item.cost * item.quantity;
+
+
+          });
+          return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+     onContinueShopping();
   };
 
-
+ const handleCheckoutShopping = (e) => {
+     alert('Functionality to be added for future reference');
+ }
 
   const handleIncrement = (item) => {
+      dispatch(updateQuantity({name:item.name,quantity:item.quantity +1}))
   };
 
   const handleDecrement = (item) => {
-   
+       if(item.quantity > 1){
+          dispatch(updateQuantity({name:item.name,quantity:item.quantity -1}))
+       }
+       else{
+         dispatch(removeItem(item.name));
+       }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+     const totalcost = parseFloat(item.cost.substring(1)) * item.quantity;
+     return totalcost;
   };
 
   return (
